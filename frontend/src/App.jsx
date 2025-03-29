eac0bf191bef5542cdb6653fdc8458ce5c645eaa
riyazedd <riyaz.shrestha00@gmail.com>
2025-03-29 19:41:59 +0545

    framer motion added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import HomePage from './pages/HomePage'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import {motion} from 'motion/react'
 
 const App = () => {
+  const location=useLocation();
   return (
     <>
     <Navbar />
     <div>
+    <motion.div key={location.pathname} initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} transition={{duration:1}}>
       <Outlet />
+      </motion.div> 
     </div>
     </>
   )
