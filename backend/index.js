fe172ccc544792de6f9808db7391fd576a3d6389
riyazedd <riyaz.shrestha00@gmail.com>
2025-08-21 11:03:19 +0545

    first commit

--- backend/index.js
+++ backend/index.js
@@ -35,29 +35,17 @@ checkAndUpdateSubscriptionDeliveries();
 
 
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  );
-} else {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+app.get('/',(req,res)=>{
+    res.send('API is running')
+})
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/upload', uploadRoutes);
 app.use('/api/orders', orderRoutes);
 
-// const __dirname = path.resolve();
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.listen(PORT,()=>console.log("Server running on port: "+PORT))
 
